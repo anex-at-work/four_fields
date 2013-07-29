690f9f8ef22c11b0cb5a2aee04e96d8b12221c22
Alex Anzelm <anex.work@gmail.com>
2013-07-29 15:50:12 +0400

    Bug fix in generators (column name and table name)

--- lib/generators/templates/four_fields_change.rb
+++ lib/generators/templates/four_fields_change.rb
@@ -1,8 +1,8 @@
-class AddFourFields<%= model_name.pluralize %> < ActiveRecord::Migration
+class AddFourFieldsTo<%= model_name.pluralize %> < ActiveRecord::Migration
   def change
     add_column :<%= model_name.tableize %>, :start_at, :datetime, :null => false
     add_column :<%= model_name.tableize %>, :end_at, :datetime, :default => nil
     add_column :<%= model_name.tableize %>, :creator_id, :integer, :null => false
-    add_column :<%= model_name.tableize %>, :end_at, :integer, :default => nil
+    add_column :<%= model_name.tableize %>, :destroyer_id, :integer, :default => nil
   end
 end
