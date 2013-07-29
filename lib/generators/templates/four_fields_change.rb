bce16f835313ecd5540bf5536ec351b4a1f454a2
Alex Anzelm <anex.work@gmail.com>
2013-07-29 15:41:29 +0400

    Another bug fix in generator Change version to 0.1.1

--- lib/generators/templates/four_fields_change.rb
+++ lib/generators/templates/four_fields_change.rb
@@ -1,4 +1,4 @@
-class AddFourFields<%= model_name %> < ActiveRecord::Migration
+class AddFourFields<%= model_name.pluralize %> < ActiveRecord::Migration
   def change
     add_column :<%= model_name.tableize %>, :start_at, :datetime, :null => false
     add_column :<%= model_name.tableize %>, :end_at, :datetime, :default => nil
