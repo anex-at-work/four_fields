9444963e0be1d8263f34a73093f35127517ef118
Alex Anzelm <anex.work@gmail.com>
2013-07-29 14:35:13 +0400

    Fixed model_name in template

--- lib/generators/templates/four_fields_change.rb
+++ lib/generators/templates/four_fields_change.rb
@@ -1,8 +1,8 @@
 class <%= model_name %> < ActiveRecord::Migration
   def change
-    add_column <%= table_name.tableize %>, :start_at, :datetime, :null => false
-    add_column <%= table_name.tableize %>, :end_at, :datetime, :default => nil
-    add_column <%= table_name.tableize %>, :creator_id, :integer, :null => false
-    add_column <%= table_name.tableize %>, :end_at, :integer, :default => nil
+    add_column <%= model_name.tableize %>, :start_at, :datetime, :null => false
+    add_column <%= model_name.tableize %>, :end_at, :datetime, :default => nil
+    add_column <%= model_name.tableize %>, :creator_id, :integer, :null => false
+    add_column <%= model_name.tableize %>, :end_at, :integer, :default => nil
   end
 end
