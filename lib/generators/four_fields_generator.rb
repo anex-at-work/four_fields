923c49e51e9f53f1dc95e2a5e9e1cc0452db3704
Alex Anzelm <anex.work@gmail.com>
2013-07-29 14:27:33 +0400

    Adding next_migration_number

--- lib/generators/four_fields_generator.rb
+++ lib/generators/four_fields_generator.rb
@@ -5,6 +5,10 @@ class FourFieldsGenerator < Rails::Generators::Base
   argument :model_name, :type => :string
   source_root File.expand_path('../templates', __FILE__)
   
+  def self.next_migration_number(path)
+    Time.now.utc.strftime("%Y%m%d%H%M%S")
+  end
+  
   def create_migration_file
     migration_template 'four_fileds_change.rb', %(db/migrate/add_four_fileds_to_#{model_name.tableize}.rb)
   end
